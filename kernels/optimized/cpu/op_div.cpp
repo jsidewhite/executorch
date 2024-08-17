c8000191a35c834149c6f8dd2bec0c04f712e63a
Jeff Whiteside <jwhites@microsoft.com>
2024-08-16 21:34:14 -0700

    Revert "[optimized_kernels] op_<x> - blackhole op code to get around heap space bug."
    This reverts commit 664571464ff7ef8afbc38336ac18da0c63a455d2.

--- kernels/optimized/cpu/op_div.cpp
+++ kernels/optimized/cpu/op_div.cpp
@@ -81,6 +81,20 @@ Tensor& opt_div_out(
       ET_SWITCH_REAL_TYPES_AND(Bool, b_type, ctx, "div.out", CTYPE_B, [&]() {
         ET_SWITCH_REAL_TYPES_AND(
             Bool, common_type, ctx, "div.out", CTYPE_IN, [&]() {
+              ET_SWITCH_REAL_TYPES_AND(
+                  Bool, out_type, ctx, "div.out", CTYPE_OUT, [&]() {
+                    apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
+                        [](const CTYPE_A val_a, const CTYPE_B val_b) {
+                          CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
+                          CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
+                          CTYPE_IN value = a_casted / b_casted;
+
+                          return static_cast<CTYPE_OUT>(value);
+                        },
+                        a,
+                        b,
+                        out);
+                  });
             });
       });
     });
@@ -109,6 +123,19 @@ Tensor& opt_div_scalar_out(
 
   if (a_type == common_type && a_type == out_type) {
     ET_SWITCH_REAL_TYPES(a_type, ctx, "div.Scalar_out", CTYPE, [&]() {
+      ET_SWITCH_REAL_TYPES_AND(
+          Bool, b_type, ctx, "div.Scalar_out", CTYPE_B, [&]() {
+            CTYPE_B b_val;
+            ET_EXTRACT_SCALAR(b, b_val);
+            CTYPE b_casted = static_cast<CTYPE>(b_val);
+
+            using Vec = executorch::vec::Vectorized<CTYPE>;
+            executorch::vec::map<CTYPE>(
+                [b_casted](Vec x) { return x / Vec(b_casted); },
+                out.mutable_data_ptr<CTYPE>(),
+                a.const_data_ptr<CTYPE>(),
+                out.numel());
+          });
     });
   } else {
     ET_SWITCH_REAL_TYPES_AND(
@@ -119,6 +146,18 @@ Tensor& opt_div_scalar_out(
                     common_type, ctx, "div.Scalar_out", CTYPE_IN, [&]() {
                       ET_SWITCH_REAL_TYPES(
                           out_type, ctx, "div.Scalar_out", CTYPE_OUT, [&]() {
+                            CTYPE_B b_val;
+                            ET_EXTRACT_SCALAR(b, b_val);
+                            CTYPE_IN b_casted = static_cast<CTYPE_IN>(b_val);
+
+                            const size_t n = a.numel();
+                            const CTYPE_A* a_data = a.const_data_ptr<CTYPE_A>();
+                            CTYPE_OUT* out_data =
+                                out.mutable_data_ptr<CTYPE_OUT>();
+                            for (auto i = 0; i < n; ++i) {
+                              out_data[i] = static_cast<CTYPE_OUT>(
+                                  static_cast<CTYPE_IN>(a_data[i]) / b_casted);
+                            }
                           });
                     });
               });
